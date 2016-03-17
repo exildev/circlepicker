e627dbf2268a4a58090d6546f078509882b4207e
Cristian Trujillo <ctrujillozt@gmail.com>
2016-03-17 12:35:59 -0500

    archivos base

---
+++ js/general.js
@@ -0,0 +1,59 @@
+$(document).ready(function(){
+    $("#rest").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '-35deg'
+            });
+        }, function (){
+    });
+    $("#bar").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '-8deg'
+            });
+        }, function (){
+    });
+    $("#tur").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '13deg'
+            });
+        }, function (){
+    });
+    $("#hote").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '40deg'
+            });
+        }, function (){
+    });
+    $("#viaj").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '67deg'
+            });
+        }, function (){
+    });
+    $("#crus").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '90deg'
+            });
+        }, function (){
+    });
+    $("#bell").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '115deg'
+            });
+        }, function (){
+    });
+    $("#espe").hover(
+        function (){        
+            $("#apuntador").transition({
+                rotate: '152deg'
+            });
+        }, function (){
+    });
+});
+
